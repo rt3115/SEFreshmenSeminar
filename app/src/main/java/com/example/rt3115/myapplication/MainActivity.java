e1de02308003dc13bb37e49bf3b4b9daf501c52c
rt3115 <Maggie4182>
2018-10-02 14:37:02 -0400

    no

--- app/src/main/java/com/example/rt3115/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rt3115/myapplication/MainActivity.java
@@ -9,11 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Hello
-        //why no dis wok big oof
-        //dfmhsfd
-        //hahahahhaha
-        //lmao
-        //tet
+
     }
 }
