d9f6c43e8a042ddfb33190992ef6d936458cb868
Mark <mam3745@g.rit.edu>
2018-10-02 15:42:35 -0400

    -Mark

---
+++ app/src/main/java/com/example/rt3115/myapplication/activity_userid_2.java
@@ -0,0 +1,32 @@
+package com.example.rt3115.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class activity_userid_2 extends AppCompatActivity {
+    private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userid_2);
+
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
