d24dc29f8d8c6dd11bc2b9dc2775fe59bda5aa3a
jds9821 <jds9821@rit.edu>
2018-10-02 15:26:26 -0400

    cooment

---
+++ app/src/main/java/com/example/rt3115/myapplication/activity_userid_2.java
@@ -0,0 +1,15 @@
+package com.example.rt3115.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_userid_2 extends AppCompatActivity {
+    private Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userid_2);
+
+        button2 = findViewById(R.id.button2);
+    }
+}
